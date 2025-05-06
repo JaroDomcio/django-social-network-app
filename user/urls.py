661cc6b1c66cf40361b27da6d8cfc472a72b5d0f
JaroDomcio <281053@student.pwr.edu.pl>
2025-05-06 21:29:37 +0200

    Improve code readability in user/urls.py and improved logout button

--- user/urls.py
+++ user/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path, include
 from .views import register,home
+from django.contrib.auth.views import LoginView,LogoutView
 
 urlpatterns = [
     path('register/', register, name='register'),
-    path('', include('django.contrib.auth.urls')),
+    path('login/', LoginView.as_view(), name='login'),
+    path('logout/', LogoutView.as_view(), name='logout'),
     path('home/' , home , name='home'),
 ]
