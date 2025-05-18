cc115773c1a11b52d0aec9aab048d14888630da4
JaroDomcio <281053@student.pwr.edu.pl>
2025-05-18 22:17:07 +0200

    Now users can visit other users profiles

--- profiles/views.py
+++ profiles/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from .models import Profile
+from post.models import Post
 
-# Create your views here.
+
+def profile(request,id):
+    users_profile = Profile.objects.get(id = id)
+    posts = Post.objects.filter(user = users_profile.user)
+    return render(request, 'profile.html',{'profile':users_profile,'posts':posts})
