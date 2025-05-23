83cab4917874d98c0bb32b95a1b590ee445e834c
JaroDomcio <281053@student.pwr.edu.pl>
2025-05-23 12:54:58 +0200

    Updated fronted for comments logic

--- user/views.py
+++ user/views.py
@@ -1,7 +1,7 @@
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, redirect, get_object_or_404
 from post.models import Post
-from profiles.models import Profile
+from comment.models import Comment
 from .forms import *
 
 def register(request):
